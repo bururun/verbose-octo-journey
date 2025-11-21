8b10e135861ec3e9fba856467c9df47c7680187a
bururun <bururun@users.noreply.github.com>
2025-11-22 00:58:40 +0200

    improve: improve code readability

---
+++ lib/helpers.py
@@ -0,0 +1,9 @@
+# Helper functions
+
+def helper_function_20(x):
+    """Helper function for iteration 20."""
+    return x * 20
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
